178c717bcf9300fce3bc3bad072027da0e4891be
pixellos <pixellos@users.noreply.github.com>
2015-08-27 20:44:27 +0200

    Restored

---
+++ Common Files/BackgroundConnectionHelper.cs
@@ -0,0 +1,59 @@
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System;
+using System.Windows;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Common
+{
+    /// <summary>
+    /// If u want get Backgroundworker at loop, at updateGui cal (sender as BackgroundWorker).RunWorkerAsync();
+    /// </summary>         
+    public class BackgroundConnectionHelper
+    {
+        BackgroundWorker backgroundWorker;
+
+        public BackgroundConnectionHelper(DoWorkEventHandler AsyncDelegate, RunWorkerCompletedEventHandler GUIDelegate, ProgressChangedEventHandler ProgressChanged)
+        {
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += AsyncDelegate;
+            backgroundWorker.RunWorkerCompleted += GUIDelegate;
+            backgroundWorker.ProgressChanged += ProgressChanged;
+            
+        }
+
+        public BackgroundConnectionHelper(DoWorkEventHandler AsyncDelegate, RunWorkerCompletedEventHandler GUIDelegate)
+        {
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += AsyncDelegate;
+            backgroundWorker.RunWorkerCompleted += GUIDelegate;
+            backgroundWorker.WorkerSupportsCancellation = true;
+        }
+
+        public void Start()
+        {
+            if (IsNotReady)
+            {
+                Stop();
+            }
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        public void Stop() { backgroundWorker.CancelAsync(); }
+
+        public bool IsNotReady
+        {
+            get
+            {
+                return backgroundWorker.IsBusy;
+            }
+        }
+    }
+}
